c874f5cbee358f81a70d2c563e932f06557b5c23
m-rokh <m.raha988@gmail.com>
2022-08-17 08:12:06 +0430

    created splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,11 @@ import 'package:my_teckblog/my_colors.dart';
 import 'package:my_teckblog/splash_screen.dart';
 
 void main() {
-
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    
-    statusBarColor: SolidColors.statusBarColor,
-    statusBarIconBrightness: Brightness.dark,
-    systemNavigationBarColor: SolidColors.systemNavigationBarColor,
-    systemNavigationBarIconBrightness: Brightness.dark
-    
-  ));
+      statusBarColor: SolidColors.statusBarColor,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: SolidColors.systemNavigationBarColor,
+      systemNavigationBarIconBrightness: Brightness.dark));
 
   runApp(const MyApp());
 }
@@ -24,99 +20,80 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     var textTheme = Theme.of(context).textTheme;
 
-
     return MaterialApp(
-      localizationsDelegates: const [
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-           ],
-      supportedLocales: const [
-        Locale('fa', ''), // farsi
-      ],
-      theme: ThemeData(
-
-
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('fa', ''), // farsi
+        ],
+        theme: ThemeData(
           inputDecorationTheme: InputDecorationTheme(
-
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16),
-              borderSide: const BorderSide(width: 2),
-              
-              
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+                borderSide: const BorderSide(width: 2),
               ),
               filled: true,
-              fillColor: Colors.white
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(style:  ButtonStyle(
-                  textStyle: MaterialStateProperty.resolveWith((states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return textTheme.headline1;
-                    }
+              fillColor: Colors.white),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+            textStyle: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return textTheme.headline1;
+              }
 
-                    return textTheme.subtitle1;
-                  }),
-                  backgroundColor: MaterialStateProperty.resolveWith((states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return SolidColors.seeMore;
-                    }
+              return textTheme.subtitle1;
+            }),
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return SolidColors.seeMore;
+              }
 
-                    return SolidColors.primeryColor;
-                  }),
-                )),
-
-
-
+              return SolidColors.primeryColor;
+            }),
+          )),
           fontFamily: 'dana',
           brightness: Brightness.light,
           textTheme: const TextTheme(
-
-              headline1: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: SolidColors.posterTitle),
-
-             subtitle1: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w300,
-                  color: SolidColors.posterSubTitle),
-
-              bodyText1: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 13,
-                  fontWeight: FontWeight.w300),
-              headline2: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 14,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w300),
-              headline3: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 14,
-                  color: SolidColors.seeMore,
-                  fontWeight: FontWeight.w700),
-              headline4: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 14,
-                  color: Color.fromARGB(255, 70, 70, 70),
-                  fontWeight: FontWeight.w700),
-              headline5: TextStyle(
-                  fontFamily: 'dana',
-                  fontSize: 14,
-                  color: SolidColors.hintText,
-                  fontWeight: FontWeight.w700),
-    
-                  
-                  
-                  )),
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen()
-      
-    );
+            headline1: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                color: SolidColors.posterTitle),
+            subtitle1: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+                color: SolidColors.posterSubTitle),
+            bodyText1: TextStyle(
+                fontFamily: 'dana', fontSize: 13, fontWeight: FontWeight.w300),
+            headline2: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w300),
+            headline3: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 14,
+                color: SolidColors.seeMore,
+                fontWeight: FontWeight.w700),
+            headline4: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 14,
+                color: Color.fromARGB(255, 70, 70, 70),
+                fontWeight: FontWeight.w700),
+            headline5: TextStyle(
+                fontFamily: 'dana',
+                fontSize: 14,
+                color: SolidColors.hintText,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: splashScreen());
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:my_teckblog/gen/assets.gen.dart';
+import 'package:my_teckblog/my_colors.dart';
 
-
-class SplashScreen extends StatefulWidget{
+class splashScreen extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage(Assets.images.logo.path),
+                height: 64,
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              SpinKitCircle(
+                color: SolidColors.primeryColor,
+                size: 50.0,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
