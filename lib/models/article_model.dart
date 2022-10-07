4ef71b81e156908a1dcb36fdd0f25356c7e7cb72
m-rokh <m.raha988@gmail.com>
2022-10-07 12:43:04 +0330

    updateArticleModel

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,40 +1,38 @@
 class ArticleModel {
-  String? id;
-  String? title;
-  String? image;
-  String? cat_id;
-  String? cat_name;
-  String? author;
-  String? view;
-  String? status;
-  String? created_at;
+   String? id;
+   String? title;
+   String? image;
+   String? catId;
+   String? catName;
+   String? author;
+   String? view;
+   String? status;
+   String? createdAt;
 
   ArticleModel({
     required this.id,
     required this.title,
     required this.image,
-    required this.cat_id,
-    required this.cat_name,
+    required this.catId,
+    required this.catName,
     required this.author,
     required this.view,
     required this.status,
-    required this.created_at,
+    required this.createdAt,
     
     
     
     });
+
+    ArticleModel.fromJson(Map<String,dynamic> element){
+   id = element["id"];
+   title = element["title"];
+   image = element["image"];
+   catId = element["cat_id"];
+   catName = element["cat_name"];
+   author = element["author"];
+   view = element["view"];
+   status = element["status"];
+   createdAt = element["created_at"];
+    }
 }
-
-
-
-
-
-            // "id": "16",
-            // "title": "اطلاعات جدیدی از بازی Assassin's Creed Mirage فاش شد",
-            // "image": "/Techblog/assets/upload/images/article/20220904181457.jpg",
-            // "cat_id": "3",
-            // "cat_name": "اخبار بازی",
-            // "author": "محسن چگینی",
-            // "view": "987",
-            // "status": "1",
-            // "created_at": "۱۴۰۱/۶/۱۲"
