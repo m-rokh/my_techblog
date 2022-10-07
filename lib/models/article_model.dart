9543ee5738ad82de10875dac17d03d869e86ac82
m-rokh <m.raha988@gmail.com>
2022-10-07 12:23:47 +0330

    createdArticleModel

---
+++ lib/models/article_model.dart
@@ -0,0 +1,40 @@
+class ArticleModel {
+  String? id;
+  String? title;
+  String? image;
+  String? cat_id;
+  String? cat_name;
+  String? author;
+  String? view;
+  String? status;
+  String? created_at;
+
+  ArticleModel({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.cat_id,
+    required this.cat_name,
+    required this.author,
+    required this.view,
+    required this.status,
+    required this.created_at,
+    
+    
+    
+    });
+}
+
+
+
+
+
+            // "id": "16",
+            // "title": "اطلاعات جدیدی از بازی Assassin's Creed Mirage فاش شد",
+            // "image": "/Techblog/assets/upload/images/article/20220904181457.jpg",
+            // "cat_id": "3",
+            // "cat_name": "اخبار بازی",
+            // "author": "محسن چگینی",
+            // "view": "987",
+            // "status": "1",
+            // "created_at": "۱۴۰۱/۶/۱۲"
